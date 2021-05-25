a5734781bd86f4a120ccc22bffeebe9352fb5ec4
goaldeer <goaldeer@gmail.com>
2021-05-25 06:06:50 +0000

    11

---
+++ src/project/Book.java
@@ -0,0 +1,239 @@
+package project;
+
+import java.util.*;
+
+public class Book {
+    
+    Scanner sc = new Scanner(System.in);
+    private String bNo;
+    private String bTitle;
+    private String bAuthor;
+    private String bPrice;
+    public static ArrayList<Book> bookList = new ArrayList<Book>();
+ 
+    public Book() {
+ 
+    }
+ 
+    public String getbNo() {
+        return bNo;
+    }
+ 
+    public void setbNo(String bNo) {
+        this.bNo = bNo;
+    }
+ 
+    public String getbTitle() {
+        return bTitle;
+    }
+ 
+    public void setbTitle(String bTitle) {
+        this.bTitle = bTitle;
+    }
+ 
+    public String getbAuthor() {
+        return bAuthor;
+    }
+ 
+    public void setbAuthor(String bAuthor) {
+        this.bAuthor = bAuthor;
+    }
+ 
+    public String getbPrice() {
+        return bPrice;
+    }
+ 
+    public void setbPrice(String bPrice) {
+        this.bPrice = bPrice;
+    }
+ 
+    
+    public void showMain() throws InterruptedException {
+    while (true) {
+        System.out.format("현재 [%d]권의 도서가 등록되어 있습니다.\n", bookList.size());
+        System.out.format("등록된 도서의 평균 가격은 %d원 입니다.");
+        System.out.println("[1] 도서 등록\n[2] 등록 도서 조회\n[3] 도서 정보 업데이트\n[4] 도서 삭제\n[5] 프로그램 종료\n");
+        String userInput = sc.nextLine();
+ 
+        switch (userInput) {
+        case ("1"):
+            insertBook();
+            break;
+ 
+        case ("2"):
+            selectAll();
+            break;
+                
+        case ("3"):
+            updateBook();
+            break;
+ 
+        case ("4"):
+            deletebook();
+            break;
+                
+        case ("5"):
+            System.out.println("프로그램 종료");
+            sc.close();
+            System.exit(0);
+                
+        default:
+            System.out.println("잘못 입력하셨습니다. 다시 입력하세요.");
+            break;
+        }
+    } // end while
+    }
+
+    public void insertBook() throws InterruptedException {
+        while (true) {
+            Book book = new Book();
+            // 도서 번호는 중복되면 안 됨
+            while (true) {
+                int cnt=0;
+                System.out.println("도서 번호 입력");
+                String temp = sc.nextLine();
+                for (int i = 0; i < bookList.size(); i++) {
+                    if (temp.equals(bookList.get(i).getbNo())) {
+                        cnt++;
+                        System.out.println("도서 번호 중복입니다. 다시 입력하세요.");
+                        break;
+                    } // end if
+                } // end for
+                if(cnt==0) {
+                    book.setbNo(temp);
+                    break;
+                }
+            } // end while
+            System.out.println("책 제목 입력");
+            book.setbTitle(sc.nextLine());
+            
+            System.out.println("작가 입력");
+            book.setbAuthor(sc.nextLine());
+            
+            System.out.println("가격 입력");
+            book.setbPrice(sc.nextLine());
+            
+            Thread.sleep(1000);
+ 
+            // 컨펌 후에 리스트에 객체 저장
+            System.out.println("도서번호 : " + book.getbNo());
+            System.out.println("도서제목 : " + book.getbTitle());
+            System.out.println("지 은 이  : " + book.getbAuthor());
+            System.out.println("가격  : " + book.getbPrice());
+ 
+            System.out.println("입력하신 사항이 모두 맞습니까? 예(Y) 아니오(N)");
+            String confirm = sc.nextLine();
+            if (confirm.equalsIgnoreCase("y")) {
+                bookList.add(book);
+                System.out.println("======입력 완료=====");
+                break;
+            } else if (confirm.equalsIgnoreCase("n")) {
+                System.out.println("도서 정보를 새로 입력하세요.");
+            } else {
+                System.out.println("잘못 누르셨습니다. 초기 메뉴로 이동합니다");
+                break; // 초기메뉴로 이동
+            } // if-else end
+        } // end while
+ 
+        Thread.sleep(1000);
+    }// end main
+    
+    public void selectAll() throws InterruptedException {
+        while (true) {
+            System.out.println("등록 도서량:  " + bookList.size());
+            for (int i = 0; i < bookList.size(); i++) {
+                System.out.println("===============================");
+                System.out.println("책 번호 : " + bookList.get(i).getbNo());
+                System.out.println("책 제목 : " + bookList.get(i).getbTitle());
+                System.out.println("지은이  : " + bookList.get(i).getbAuthor());
+                System.out.println("가격  : " + bookList.get(i).getbPrice());
+                System.out.println("===============================\n");
+ 
+            } // end for
+            System.out.println("초기 메뉴 이동 : [1] \n프로그램 종료 : [0]");
+            String temp = sc.nextLine();
+            if (temp.equalsIgnoreCase("1")) {
+                break;
+            } else if (temp.equals("0")) {
+                System.out.println("프로그램을 종료합니다.");
+                Thread.sleep(1000);
+                System.exit(0);
+            } else {
+                System.out.println("잘못 누르셨습니다. 초기화면으로 이동합니다.");
+                Thread.sleep(1000);
+                break; // 메인으로 돌아감
+            } // if-else end
+        } // while end
+    }// end selectAll
+ 
+    public void updateBook() {
+        
+        while (true) {
+            
+            System.out.println("수정 할 책의 번호를 입력해주세요");
+            String temp = sc.nextLine();
+            int cnt = 0;
+            
+            for (int i = 0; i < bookList.size(); i++) {
+                
+                if (temp.equals(bookList.get(i).getbNo())) {
+                    
+                    System.out.println("새로운 제목 입력: ");
+                    bookList.get(i).setbTitle(sc.nextLine());
+                    
+                    System.out.println("새로운 지은이 입력: ");
+                    bookList.get(i).setbAuthor(sc.nextLine());
+                    
+                    System.out.println("새로운 가격 입력: ");
+                    bookList.get(i).setbPrice(sc.nextLine());
+                    cnt++;
+                    System.out.println("도서 수정 완료");
+                    break;
+                }
+                
+            } // end for
+            
+            if (cnt == 0) {
+                
+                System.out.println("해당 도서가 존재하지 않습니다. 도서 번호를 다시 입력하세요.");
+                
+            } else {
+                
+                break;// 메인으로
+                
+            }
+        } // end while
+    }
+    
+    public void deletebook() {
+        while (true) {
+            
+            System.out.println("삭제 할 책의 번호를 입력해주세요");
+            
+            String temp = sc.nextLine();
+            
+            int cnt = 0;
+            
+            for (int i = 0; i < bookList.size(); i++) {
+                
+                if (temp.equals(bookList.get(i).getbNo())) {
+                    
+                    bookList.remove(i);
+                    cnt++;
+                    System.out.println("도서 삭제 완료");
+                    
+                    break;
+                    
+                }
+            } // end for
+            
+            if (cnt == 0) {
+                System.out.println("해당 도서가 존재하지 않습니다. 도서 번호를 다시 입력하세요.");
+            } else {
+                break;// 메인으로
+            }
+        } // end while
+    } 
+    
+}
+
--- src/project/Main.java
+++ src/project/Main.java
@@ -1,312 +1,95 @@
 //jinhong@inje.ac.kr
 
 package project;
-import java.io.RandomAccessFile;
-import java.io.*;
-import java.util.Scanner;
-import java.util.ArrayList;
+
 import java.util.*;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.charset.Charset;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
-import java.text.SimpleDateFormat;
-import java.nio.file.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-
-/* 
-
-1. 지정된 경로의 모든 파일명을 읽어 리스트로 저장하기. File[]
-
-2. 0번 인덱스 부터 시작해서 파일을 순서대로 읽기
-2-1. 파싱 프로그램을 이용해서 c - l - a - s - s 를 읽을 시에, 공백을 기준으로 하여 그 뒤의 이름 읽어와 List에 넣기.
-
-*/
 
 public class Main {
     
-    static File[] workingFiles;
-    static int count = 0;
-    static Scanner scan;
-    static ArrayList<File> studentList = new ArrayList<File>();
-    static String[][] Scores;
-    
-    public static void main(String args[]) {
+    public static void main(String[] args) throws InterruptedException {
         
-        long systemTime = System.currentTimeMillis(); 
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd", Locale.KOREA);
-        String dTime = formatter.format(systemTime);
-    
-        String workingPath = Paths.get("HIT_Java", dTime).toAbsolutePath().toString();
-        String inputPath = workingPath + File.separatorChar + "Inputs"; //폴더 경로
-
-        
-        if (makeFolder(inputPath)) {
-            System.out.println("Inputs 폴더 생성 완료, zip 파일들을 알맞은 일자의 폴더에 넣어주세요.");
-            System.out.printf("모두 옮겼으면,");
-        }
-    
-        while (true) {
-            scan = new Scanner(System.in);
-            String dummyData = null;
-        
-            try {
-                System.out.println("아무 키나 입력 후 엔터를 눌러 주세요...");
-                dummyData = scan.nextLine();
-                
-            } catch (java.util.InputMismatchException e) {
-                continue;
-            }
-            
-        
-            File f = new File(inputPath);
-            File[] fileList = f.listFiles();
-            
-            try {
-                
-                if (fileList.length == 0) {
-                    scan = new Scanner(System.in);
-                    throw new Exception("압축파일이 존재하지 않습니다.\n");
-                }
-                
-                for (File file : fileList) {
-                    if (file.isFile()) {
-                        String fileName = file.getAbsolutePath();
-                        if (unZip(fileName, workingPath + File.separatorChar + "working" + File.separatorChar + file.getName().substring(0, file.getName().lastIndexOf(".")))) {
-                            
-                        }
-                    }
-                }
-                
-                scan.close();
-                break;
-            } catch (Exception e) {
-                System.out.println("\n\nERROR : " + e + "\n다시 시도해 주세요..");
-                continue;
-            }
-        }
-        
-        System.out.println("\n\n\n제출자 명단 - \n");
-        
-        for (File ff : studentList) {
-            System.out.println(ff.getName());
-        }
-        
-        System.out.println("채점 DONE");
-        
+        Book book = new Book();
+        book.showMain();
     }
     
-    static int classCounter = 0;
-    private static void ClassReader(File fi) {
-        
+    public static void writer() throws InterruptedException {
         try {
-            
-                ArrayList<String> classList = new ArrayList<String>();
-                String str = null;
-                String ss = "";
-                    
-                char[] c = new char[(int)fi.length()];
-                BufferedReader br = new BufferedReader(new FileReader(fi));
-                br.read(c);
-            
-                for(int i = 0; i < c.length; i++) {
-                    if (c[i] == 'c') {
-                        if (c[i + 1] == 'l') {
-                            if (c[i + 2] == 'a') {
-                                if (c[i + 3] == 's') {
-                                    if (c[i + 4] == 's') {
-                                        int cc = 6;
-                                        ss = "";
-                                        while (true) {
-                                            if (c[i + cc] == ' ') {
-                                                classList.add(ss);
-                                                classCounter++;
-                                                break;
-                                            }
-                                            ss = ss + String.valueOf(c[i + cc]);
-                                            cc++;
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-                
-                for (String s : classList) {
-                    System.out.println(s);
-                }
-                
-                str = str + String.valueOf(c);
-                //System.out.print(str);
-                br.close();
-                
-            } catch ( IOException e ) {
-                System.out.println(e);
-            }
-            
-            if (fi.isFile()) {
-                //System.out.println("채점완료..");
-            }
-        
-    }
-    
-    //안씀
-    private static File[] ListLayerFile(String strDirPath) {
-        File path = new File(strDirPath); 
-        File[] fList = path.listFiles();
-        ArrayList<File> result = new ArrayList<File>();
-        
-        for(File fl : fList) { 
-            if(fl.isFile()) { 
-                System.out.println(fl.getPath()); // 파일의 FullPath 출력
-                result.add(fl);
-            } else if(fl.isDirectory()) { 
-                ListLayerFile(fl.getPath()); // 재귀함수 호출 
-            } 
-        } 
-        
-        return fList;
-    }
+            String createfile="/workspace/JAVA_study/csvfile.csv";
+            FileWriter fw = new FileWriter(createfile);
 
-    
-    public static boolean unZip(String zipPath, String zipUnzipPath) {
-        int zipCount = 0;
-        classCounter = 0;
-        
-        // 파일 정상적으로 압축이 해제가 되어는가. 
-        boolean isChk = false; 
-        // 해제할 홀더 위치를 재조정 
-        zipUnzipPath = zipUnzipPath.replace(".zip", ""); 
-        // zip 파일 
-        File zipFile = new File(zipPath); 
-        FileInputStream fis = null; 
-        ZipInputStream zis = null; 
-        ZipEntry zipentry = null; 
-        File ffile = new File(zipUnzipPath);
-        
-        
-        
-        try {
+            fw.append("NO");
+            fw.append(',');
+            fw.append("TIME");
+            fw.append(',');
+            fw.append("Vib1");
+            fw.append('\n');
             
-            System.out.println("\n" + zipFile.getName().substring(0, zipFile.getName().lastIndexOf(".")) + " ---------------- ");
-            
-            // zipFileName을 통해서 폴더 만들기 
-            if (makeFolder(zipUnzipPath)) { 
-                //System.out.println("압축 해제 경로 설정중...");       
-                studentList.add(ffile);
-            } 
-            // 파일 스트림 
-            fis = new FileInputStream(zipFile); 
-            // Zip 파일 스트림 
-            zis = new ZipInputStream(fis, Charset.forName("EUC-KR")); 
-            // 압축되어 있는 ZIP 파일의 목록 조회 
-            while ((zipentry = zis.getNextEntry()) != null) { 
-                String filename = zipentry.getName(); 
-                //System.out.println("압축 해제중... => " + filename); 
-                File file = new File(zipUnzipPath, filename); 
-                // entiry가 폴더면 폴더 생성 
-                if (zipentry.isDirectory()) { 
-                    //System.out.println("zipentry가 디렉토리입니다."); 
-                    file.mkdirs(); 
-                } else { 
-                    // 파일이면 파일 만들기 
-                    //System.out.println("zipentry가 파일입니다."); 
-                    try { 
-                        createFile(file, zis);
-                        System.out.println("\n파일 용량 : " + String.valueOf(file.length()) + " byte");
-                        ClassReader(file);
-                        zipCount++;
-                        
-                    } catch (Throwable e) {
-                        e.printStackTrace();
-                    } 
-                } 
-            }
-            
-            isChk = true; 
-            System.out.println(zipFile.getName().substring(0, zipFile.getName().lastIndexOf(".")) + " : " + String.valueOf(zipCount) + "개의 파일, " + String.valueOf(classCounter) + "개의 클래스.\n");
-        } catch (Exception e) { 
-            isChk = false; 
-            
-        } finally { 
-            if (zis != null) { 
-                try { zis.close(); 
-                    } catch (IOException e) {
-                } 
-            } 
-            
-            if (fis != null) { 
-                try { fis.close(); 
-                    } catch (IOException e) {
-                } 
-            } 
-        } 
-        return isChk; 
-    }
-    
-    /** 
-    * @param folder - 생성할 폴더 경로와 이름 */ 
-    
-    private static boolean makeFolder(String folder) { 
-        if (folder.length() < 0) { 
-            return false; 
-        } 
-        String path = folder; 
-        
-        // 폴더 경로 
-        File Folder = new File(path); 
-        
-        // 해당 디렉토리가 없을경우 디렉토리를 생성합니다. 
-        if (!Folder.exists()) {
-            try { 
-                Folder.mkdirs(); 
+            for(true) {
+                fw.append();
+                fw.append();
                 
-                // 폴더 생성합니다. 
-                //System.out.println("폴더가 생성되었습니다.");
-            } catch (Exception e) { 
-                e.getStackTrace(); 
-            } 
-        } else { 
-            //System.out.println("이미 폴더가 생성되어 있습니다."); 
+            }
+
+            fw.flush();
+            fw.close();
+        } catch (Exception e) {
+            System.out.println(e);
         }
-        return true; 
     }
     
-    /** 
-    * 파일 만들기 메소드 * 
-    * @param file 파일 * 
-    @param zis Zip스트림 
-    */
-    
-    private static void createFile(File file, ZipInputStream zis) throws Throwable {
-        // 디렉토리 확인 
-        File parentDir = new File(file.getParent()); 
-        // 디렉토리가 없으면 생성하자
-        if (!parentDir.exists()) {
-            parentDir.mkdirs(); 
-        } 
-        FileOutputStream fos = null; 
-        // 파일 스트림 선언 
-        try { 
-            fos = new FileOutputStream(file); 
-            byte[] buffer = new byte[256]; 
-            int size = 0; 
-            // Zip스트림으로부터 byte뽑아내기 
-            while ((size = zis.read(buffer)) > 0) { 
-                // byte로 파일 만들기 
-                fos.write(buffer, 0, size); 
-            } 
-        } catch (Throwable e) {
-            throw e; 
-        } finally {
-            if (fos != null) {
-                try { 
-                    fos.close();
-                } catch (IOException e) {
+    public static void reader() {
+        String path = "/workspace/JAVA_study/csvfile.csv";
+        //List<List<String>> ret = new ArrayList<List<String>>();
+        BufferedReader br = null;
+        
+        try{
+            br = Files.newBufferedReader(Paths.get(path));
+            //Charset.forName("UTF-8");
+            String line = "";
+            
+            while((line = br.readLine()) != null){
+                //CSV 1행을 저장하는 리스트
+                List<String> tmpList = new ArrayList<String>();
+                String array[] = line.split(",");
+                //배열에서 리스트 반환
+                tmpList = Arrays.asList(array);
+                System.out.println(tmpList);
+                Book bb = new Book();
+                bb.setbNo(array[0]);
+                bb.setbTitle(array[1]);
+                bb.setbAuthor(array[2]);
+                bb.setbPrice(array[3]);
+                
+                Book.bookList.add(bb);
+                //ret.add(tmpList);
+            }
+        }catch(FileNotFoundException e){
+            e.printStackTrace();
+        }catch(IOException e){
+            e.printStackTrace();
+        }finally{
+            try{
+                if(br != null){
+                    br.close();
                 }
+            }catch(IOException e){
+                e.printStackTrace();
             }
         }
+
     }
 }
